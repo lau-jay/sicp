6301b33fa7934af6002f52e089278ed227b8dcd6
jay <cappyclear@gmail.com>
2019-10-25 14:14:00 +0800

    勘误修改习题答案

--- code/building-abstractions-with-procedures/1-3.rkt
+++ code/building-abstractions-with-procedures/1-3.rkt
@@ -1,12 +1,14 @@
 #lang sicp
-;;求3个数里最大的两个数之和
+;;求3个数里最大的两个数的平方和
 
 (define (sum a b) (+ a b))
 
+(define (square x) (* x x))
+
 (define (max_two_sum a b c)
-  (cond ((and (<= a b) (<= a c)) (sum b c))
-        ((and (<= b a) (<= b c)) (sum a c))
-        (else (sum a b))
+  (cond ((and (<= a b) (<= a c)) (sum (square b) (square c)))
+        ((and (<= b a) (<= b c)) (sum (square a) (square c)))
+        (else (sum (square a) (square b)))
         ))
 
 (max_two_sum 1 4  5)
